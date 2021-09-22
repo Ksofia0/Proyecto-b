16e381f9b6b5b93ed33c3c7975adad89da52711a
Ksofia0 <90328023+Ksofia0@users.noreply.github.com>
2021-09-22 08:10:45 -0500

    Add files via upload
    Importe el archivo de operaciones basicas a Github.

---
+++ operacionesbasicas.py
@@ -0,0 +1,10 @@
+print("este programa muestra resultados de operaciones basicas")
+print("la suma de 4444 + 5555 = ")
+print (4444 + 5555)
+print("la resta de 4444 - 5555 = ")
+4444 - 5555
+print("la multiplicacion de 4444 * 5555 = ")
+4444 * 5555
+print("la divicion de 4444 / 5555 = ")
+
+print ("fin del programa")
